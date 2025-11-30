c55e4990509bceb01115371e19edc6e6c7ed819e
PabloNicolasGarcia <pablonicoryan@gmail.com>
2025-11-30 15:53:16 +0100

    Cambio estilo Se añade JS para cambiar estilo de barberos. Se añade CSS para modificar el estilo de los botones de los barberos. Se modifica CSS para ajustar los botones todos en la misma posición. Se modifica formulario para añadir un max de caracteres en el campo de mensaje.

--- javascript.js
+++ javascript.js
@@ -20,3 +20,18 @@ document.getElementById("masreseñas").onclick = function () {
     this.style.display = "none";
 };
 
+
+document.getElementById("stylebarbero1").onclick = function () {
+    document.getElementById("barbero1").style.border = "1px solid #FFC107";
+    document.getElementById("barbero1").style.boxShadow = "0 6px 20px rgba(255, 193, 7, 0.879)";
+};
+
+document.getElementById("stylebarbero2").onclick = function () {
+    document.getElementById("barbero2").style.border = "1px solid #FFC107";
+    document.getElementById("barbero2").style.boxShadow = "0 6px 20px rgba(255, 193, 7, 0.879)";
+};
+
+document.getElementById("stylebarbero3").onclick = function () {
+    document.getElementById("barbero3").style.border = "1px solid #FFC107";
+    document.getElementById("barbero3").style.boxShadow = "0 6px 20px rgba(255, 193, 7, 0.879)";
+};
